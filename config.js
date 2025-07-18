b09661c89ba7354390dde9aecaec8581282c4e94
Bukamuso <bukamusosebata@gmail.com>
2025-07-18 16:23:01 +0200

    new pages and styling

--- config.js
+++ config.js
@@ -1,10 +1,8 @@
-
-
 let rootPath = "https://mysite.itvarsity.org/api/ContactBook/";
 let apiKey = checkApiKey();
 
 function checkApiKey() {
-    if (!localStorage.getItem("apiKey")){
+    if (!localStorage.getItem("apiKey")) {
         window.open("enter_api_key.html", "_self");
     }
     return localStorage.getItem("apiKey");
